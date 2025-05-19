17416936e0663be58380152e57ade345e37161d1
LolStar123 <AtulSwaggalicious@gmail.com>
2025-05-19 13:31:07 +0000

    clean up kageyama code

--- test1.py
+++ test1.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Generate x values
-x = np.linspace(0, 100, 100) 
+x = np.linspace(0, 100, 101) 
 
 y = 1 - (0.98)**x
 
 plt.figure(figsize=(8, 5))
 plt.plot(x, y, linewidth=2)
+
+plt.xticks(np.arange(0, 101, 10))
 
 plt.xlabel('number of pulls')
 plt.ylabel('probability of pulling at least at least one kageyama')
